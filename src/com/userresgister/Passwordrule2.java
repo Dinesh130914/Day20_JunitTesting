156d820f1097741829803eb5b78df6029e2487dc
Dinesh130914 <dineshthiyagu99@gmail.com>
2021-12-27 16:44:01 +0530

    Added:Validate Password,should start with upper case

--- src/com/userresgister/Passwordrule2.java
+++ src/com/userresgister/Passwordrule2.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Passwordrule1 {
+public class Passwordrule2 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Enter the Password");
+		System.out.println("Enter the Password(Should have atleast one uppercase)");
 		String pwd = scanner.next();
 		
-		String result = "^[A-Za-z]{8,15}";
+		String result = "^(?=.*[A-Z])[A-Za-z]{8,15}$";
 		Pattern regex = Pattern.compile(result);
 		Matcher inputMatcher = regex.matcher(pwd);
 		
@@ -20,7 +20,7 @@ public class Passwordrule1 {
 			System.out.println("Valid password");
 		}else 
 		{
-			System.out.println("Invalid password.should have minimum 8 character");
+			System.out.println("Invalid.should have atleast one uppercase");
 		}
 	}
 }
